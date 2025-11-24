1c94572f37b1bf9327c0798cdd37b234028e9a4e
dpriyanshiii <itsdpriyanshii@gmail.com>
2025-11-25 02:01:43 +0530

    the square one

---
+++ testing python imports.py
@@ -0,0 +1,14 @@
+#import sys
+#print("Current Python executable path:", sys.executable)
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print("All done!")
+
+
+
+print("matplotlib finallyyyy")
